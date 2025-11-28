fc604c0a77141873cf5df159f539c99c9f4e2f60
rchanduchinnuru-sketch <rchanduchinnuru@gmail.com>
2025-11-28 23:04:53 +0530

    nested if.c

---
+++ nested if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int number = 5;
+if(number > 0)
+{
+    if(number % 2 == 0)
+    {
+    printf("the number is even");
+}
+else
+{
+    printf("the number is odd");
+}
+}
+return 0;
+}
