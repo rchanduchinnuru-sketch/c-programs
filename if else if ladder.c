521e44a77ad2fb69bfcd07e63d8335cbc8eb442c
rchanduchinnuru-sketch <rchanduchinnuru@gmail.com>
2025-11-28 23:17:26 +0530

    if else if ladder.c

---
+++ if else if ladder.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int marks=85;
+if(marks >= 90)
+{
+    printf("a grade");
+    } else if(marks >= 80){
+        printf("b grade");
+    } else if(marks >= 70){
+        printf("c grade");
+       } else
+        {
+            printf("f grade");
+        }
+        return 0;
+    }
