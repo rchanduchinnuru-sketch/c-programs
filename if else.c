c9e41decdd1014873169fb135217349c131ca448
rchanduchinnuru-sketch <rchanduchinnuru@gmail.com>
2025-11-28 22:52:33 +0530

    if else.c

---
+++ if else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+int number = 10;
+if (number < 0)
+    {
+    printf("the number is positive");
+    }
+else
+    {
+    printf("the number is not positive");
+    }
+return 0;
+}
