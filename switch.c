67c61e96e292bc9fbb3f74fb7ccf8b335ca2a43e
rchanduchinnuru-sketch <rchanduchinnuru@gmail.com>
2025-11-30 10:15:02 +0530

    switch.c

---
+++ switch.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int day = 3;
+    switch(day){
+    case 1:
+    printf("monday");
+    break;
+    case 2:
+    printf("tuesday");
+    break;
+    case 3:
+    printf("wednesday");
+    break;
+    default:
+    printf("invalid day");
+    }
+    return 0;
+}
