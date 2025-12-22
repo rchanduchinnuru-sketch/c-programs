68bd2b2942c5758e2cd0decd8f20ad16598fef80
rchanduchinnuru-sketch <rchanduchinnuru@gmail.com>
2025-12-22 21:42:18 +0530

    factorial using for loop.c

---
+++ factorial using for loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,factorial=1;
+    printf("enter a positive integer:");
+    scanf("%d",&n);
+    if(n<0){
+        printf("factorial is not defined for negitive numbers.\n");
+    }
+    else{
+        for(int i=1; i<=n; i++)
+        {
+            factorial*=i;
+        }
+        printf("factorial of %d is %d\n",n,factorial);
+    }
+    return 0;
+}
+    
